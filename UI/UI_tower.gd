d0784849b3b857cd5a63ae71dc48f7a0782e97fd
wuyuanlch <731947552@qq.com>
2025-06-05 18:34:47 +0800

    战争迷雾
    加入了战争迷雾，在迷雾范围无法建造防御塔

--- UI/UI_tower.gd
+++ UI/UI_tower.gd
@@ -12,15 +12,26 @@ var preview_tower
 var current_tile_pos: Vector2i = Vector2i(-1, -1)
 
 
+func is_in_light_range(tile_pos: Vector2i) -> bool:
+	var mouse_pos = map.map_to_local(tile_pos)
+	var global_pos = map.to_global(mouse_pos)
+	
+	for tower_node in towers_node.get_children():
+		var light = tower_node.get_node_or_null("PointLight2D")
+		if light:
+			var light_radius = light.texture_scale * 7 * sqrt(light.height)  # 估算灯光半径
+			if global_pos.distance_to(tower_node.global_position) <= light_radius:
+				return true
+	return false
+
 func _on_gui_input(event):
 	if event is InputEventMouseMotion and event.button_mask == 1:
-		#var mouse_pos = get_global_mouse_position()
 		var mouse_pos = GlobalCamera.get_global_mouse_position()
 		var new_tile_pos = map.local_to_map(mouse_pos)
 
 		selection_map.clear()  # 清除之前的高亮
 		var tile_data = map.get_cell_tile_data(new_tile_pos)
-		var can_place = tile_data and tile_data.get_custom_data("towerable") and !has_tower(new_tile_pos)
+		var can_place = tile_data and tile_data.get_custom_data("towerable") and !has_tower(new_tile_pos) and is_in_light_range(new_tile_pos)
 		var tile_id = 5 if can_place else 6  # 可放置用5，不可用6
 		selection_map.set_cell(new_tile_pos, tile_id, Vector2i(0,0))
 		
@@ -30,6 +41,7 @@ func _on_gui_input(event):
 			preview_tower.set_process(false)
 			preview_tower.can_look=false			# 预览塔转向暂停
 			preview_tower.can_shoot=false		# 预览塔射击暂停
+			preview_tower.get_node("PointLight2D").visible=false
 			preview_towers_node.add_child(preview_tower)
 			
 			# 显示预览塔的攻击范围 
@@ -53,7 +65,7 @@ func _on_gui_input(event):
 		if current_highlighted_tile:
 			
 			var tile_data = map.get_cell_tile_data(current_highlighted_tile)
-			if tile_data and tile_data.get_custom_data("towerable") and !has_tower(current_highlighted_tile):
+			if tile_data and tile_data.get_custom_data("towerable") and !has_tower(current_highlighted_tile) and is_in_light_range(current_highlighted_tile):
 				var tempTower = tower.instantiate()
 				
 				# 当玩家松开鼠标确认放置防御塔时才添加到 "active_towers" 组内
@@ -62,6 +74,7 @@ func _on_gui_input(event):
 				towers_node.add_child(tempTower)
 				var local_pos = map.map_to_local(current_highlighted_tile)
 				tempTower.global_position = map.to_global(local_pos)
+				tempTower.get_node("PointLight2D").visible=true
 
 				# 对于实际放置的塔，默认隐藏其攻击范围 
 				# （你可以根据需要，例如在选中塔的时候再显示它）
